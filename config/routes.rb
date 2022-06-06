6b9d1dd8e2ecb8b0d8c1ca032c3034ed8d9afc6f
NaofumiOkamoto <n.okamooo@gmail.com>
2022-06-06 12:28:22 +0900

    udpate application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'tops#index'
   get '/', to: 'tops#index'
+
+  get '/about', to: 'abouts#index', as: 'about'
+  get '/service', to: 'services#index', as: 'service'
 end
